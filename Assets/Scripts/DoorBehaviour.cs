f5e7567152c53c590c177de7b79d2646e98ceb03
Potato <goldenpotato76@gmail.com>
2017-03-26 21:00:41 +0300

    Act 2
    Managed to make the animation better, but still, problems with the door
    script.

---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour {
+
+	RaycastHit hit;
+	public GameObject camera;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Physics.Raycast(camera.transform.position, camera.transform.forward * 2, out hit, 2f);
+		Debug.DrawRay(camera.transform.position, camera.transform.forward * 2, Color.green, 1f);
+
+		if (hit.transform.tag == "Door") {
+			anim = hit.transform.GetComponent<Animator>();
+
+
+
+			anim.SetBool("isOpen", true);
+
+
+		}
+	}
+}
