d1cf0e62faf48a9d0d6f7a00c21244796193307a
Potato <goldenpotato76@gmail.com>
2017-04-09 11:55:37 +0300

    Round 14

---
+++ Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawRay(transform.position, transform.forward * 1.5f, Color.cyan);
+		if (Input.GetKeyDown(KeyCode.E)) {
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, transform.forward, out hit, 1.5f)) {
+				if (hit.transform.tag == "Interactable") {
+					Debug.Log("Interaction! wololol!");
+
+					if (hit.transform.GetComponent<ObjectInteractable>().isOpen == false) {
+						hit.transform.GetComponent<ObjectInteractable>().Open();
+					} else {
+						hit.transform.GetComponent<ObjectInteractable>().Close();
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ObjectInteractable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteractable : MonoBehaviour, IInteractable{
+	public bool isOpen;
+	public string Type;
+
+	void Start () {
+		isOpen = false;
+	}
+
+	public void Open () {
+		switch (Type) {
+			case "Door": {
+				Debug.Log("door!");
+				break;
+			}
+		}
+	}
+
+	public void Close () {
+
+	}
+}
