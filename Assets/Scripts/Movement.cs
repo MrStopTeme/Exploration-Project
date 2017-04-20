427b057b19363a5abeaeccb9dc5068b7569ac2f6
Potato <goldenpotato76@gmail.com>
2017-04-20 20:46:32 +0300

    Round 17
    - old character controller
    + new, better character controller with sprint ability ( left shift)
    -old camera looking script
    + new camera looking script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	     //Variables
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F; 
+    public float gravity = 20.0F;
+
+    public Transform cam;
+	public Transform capsule;
+    private Vector3 moveDirection = Vector3.zero;
+	public Vector3 newPos;
+
+	void Start() {
+		newPos = new Vector3(0, cam.localEulerAngles.y, 0);
+		cam.localEulerAngles = new Vector3(cam.localEulerAngles.x, 0 ,cam.localEulerAngles.z);
+		capsule.localEulerAngles = newPos;
+	}
+
+    void Update() {
+
+		newPos = new Vector3(0, cam.localEulerAngles.y, 0);
+		cam.localEulerAngles = new Vector3(cam.localEulerAngles.x, 0 ,cam.localEulerAngles.z);
+		capsule.localEulerAngles = newPos;
+			
+		capsule.Rotate(new Vector3(0, 0, 0));
+
+        CharacterController controller = GetComponent<CharacterController>();
+        // is the controller on the ground?
+        if (controller.isGrounded) {
+			if (Input.GetKey(KeyCode.LeftShift)) { //sprinting
+				speed = 12f;
+			} else {
+				speed = 6f;
+			}
+
+            //Feed moveDirection with input.
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            //Multiply it by speed.
+            moveDirection *= speed;
+            //Jumping
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+             
+        }
+        //Applying gravity to the controller
+        moveDirection.y -= gravity * Time.deltaTime;
+        //Making the character move
+        controller.Move(moveDirection * Time.deltaTime);
+     }
+}
+ 
